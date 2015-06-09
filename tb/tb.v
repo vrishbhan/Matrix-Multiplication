0803b041965edd32dcac7aa58b26a69e65bbd831
vrishbhan <vrishbhan@users.noreply.github.com>
2015-06-08 22:33:43 -0700

    test bench update

--- tb/tb.v
+++ tb/tb.v
@@ -1,7 +1,6 @@
 //*****************************************************************//
 //	Testbench for MatrixMultiplication
 //	Created By - Vrishbhan Singh Sisodia
-//	Student ID - 009300980
 //	San Jose State University
 //	EE 278
 //*****************************************************************//
